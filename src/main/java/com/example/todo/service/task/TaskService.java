c2e9b4dca9161e19e6f103c8685b6e06a125a717
Resaku9 <rariariara17@gmail.com>
2025-12-08 17:01:32 +0900

    32: サービス層のTaskEntityの導入

--- src/main/java/com/example/todo/service/task/TaskService.java
+++ src/main/java/com/example/todo/service/task/TaskService.java
@@ -2,22 +2,20 @@ package com.example.todo.service.task;
 
 import java.util.List;
 
-import com.example.todo.controller.task.TaskDTO;
-
 public class TaskService {
 	
-	public List<TaskDTO> find(){
-		var task1 = new TaskDTO(
+	public List<TaskEntity> find(){
+		var task1 = new TaskEntity(
 				1L, 
 				"Springを学ぶ", 
 				"TODOアプリを作ってみる", 
-				"TODO"
+				TaskStatus.TODO
 		);
-		var task2 = new TaskDTO(
+		var task2 = new TaskEntity(
 				2L, 
 				"Springのセキュリティを学ぶ", 
 				"ログイン機能の作成", 
-				"TODO"
+				TaskStatus.DOING
 		);
 		return List.of(task1, task2);
 	}
