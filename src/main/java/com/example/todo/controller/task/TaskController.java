ed8e047c0ab2ebd749b181ffbe5260a8a2f2ec60
Resaku9 <rariariara17@gmail.com>
2025-12-11 16:13:29 +0900

    35: DIについてコメント追加(Bean登録/取得)

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -10,8 +10,11 @@ import com.example.todo.service.task.TaskService;
 public class TaskController {
 	private final TaskService taskService;
 	//private final TaskService taskService = new TaskService();
+	
 	// 依存性注入（DI: Dependency Injection）とは、
-	//“必要なオブジェクトは 自分で new せず、外部から与えてもらう” という仕組み・考え方
+	//“必要なオブジェクトは 自分で直接 new せず、外部から与えてもらう” という仕組み・考え方
+	
+	// ここでSpring が取り出して注入してくれる（＝Bean取得）
 	public TaskController(TaskService taskService) {
 		this.taskService = taskService;
 	}
--- src/main/java/com/example/todo/service/task/TaskService.java
+++ src/main/java/com/example/todo/service/task/TaskService.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-@Service
+@Service // ここでBean登録をする
 public class TaskService {
 	
 	public List<TaskEntity> find(){
