27cc8d5c5635e02f7b3b4e5f57384f12cdf810da
Resaku9 <rariariara17@gmail.com>
2025-12-24 14:18:07 +0900

    74: 作成処理にトランザクションを追加。 処理が失敗したときにタスクを追加しない

--- src/main/java/com/example/todo/service/task/TaskService.java
+++ src/main/java/com/example/todo/service/task/TaskService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.todo.repository.task.TaskRepository;
 
@@ -24,6 +25,7 @@ public class TaskService {
 		
 	}
 
+	@Transactional // 別の処理 -> 失敗 = 作成したタスクが登録されないようにする
 	public void create(TaskEntity newEntity) {
 		taskRepository.insert(newEntity);
 	}
