e412d1fd26c396ec782ee73898f02c32d9246668
Resaku9 <rariariara17@gmail.com>
2025-12-25 14:03:11 +0900

    76: 二重サブミット対策

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -67,9 +67,9 @@ public class TaskController {
 	 * @return
 	 */
 	@PostMapping("/tasks")
-	public String create(TaskFrom form, Model model) {
+	public String create(TaskFrom form) {
 		var newEntity = new TaskEntity(null, form.summary(), form.description(), TaskStatus.valueOf(form.status()));
 		taskService.create(newEntity);
-		return list(model);
+		return "redirect:/tasks";  // 二重サブミット対策
 	}
 }
