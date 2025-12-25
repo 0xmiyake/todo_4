5e03357ae36a94bda72cd6fa24775631c88e3075
Resaku9 <rariariara17@gmail.com>
2025-12-25 14:12:45 +0900

    77: TaskForm#toEntityの導入

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -6,9 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.example.todo.service.task.TaskEntity;
 import com.example.todo.service.task.TaskService;
-import com.example.todo.service.task.TaskStatus;
 
 import lombok.RequiredArgsConstructor;
 
@@ -68,8 +66,7 @@ public class TaskController {
 	 */
 	@PostMapping("/tasks")
 	public String create(TaskFrom form) {
-		var newEntity = new TaskEntity(null, form.summary(), form.description(), TaskStatus.valueOf(form.status()));
-		taskService.create(newEntity);
+		taskService.create(form.toEntity());
 		return "redirect:/tasks";  // 二重サブミット対策
 	}
 }
--- src/main/java/com/example/todo/controller/task/TaskFrom.java
+++ src/main/java/com/example/todo/controller/task/TaskFrom.java
@@ -1,9 +1,17 @@
 package com.example.todo.controller.task;
 
+import com.example.todo.service.task.TaskEntity;
+import com.example.todo.service.task.TaskStatus;
+
 public record TaskFrom(
 		String summary,
 		String description,
 		String status
 		) {
+
+	public TaskEntity toEntity() {
+		// TODO 自動生成されたメソッド・スタブ
+		return new TaskEntity(null, summary(), description(), TaskStatus.valueOf(status()));
+	}
 
 }
