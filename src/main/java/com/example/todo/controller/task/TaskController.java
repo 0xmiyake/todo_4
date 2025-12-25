947236bb1fba057f368232f24bf93c67921309e1
Resaku9 <rariariara17@gmail.com>
2025-12-25 14:44:09 +0900

    79: バリデーションの追加 必須入力の設定

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -2,6 +2,8 @@ package com.example.todo.controller.task;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -68,7 +70,11 @@ public class TaskController {
 	 * @return
 	 */
 	@PostMapping
-	public String create(TaskFrom form) {
+	public String create(@Validated TaskFrom form, BindingResult bindingResult) {  
+		// 79: 以下、空文字で入力した際、再度フォーム画面に戻る分岐処理
+		if (bindingResult.hasErrors()) { //  79:バリデーションエラーの確認
+			return "tasks/form"; // 79: form.htmlを指している
+		}
 		taskService.create(form.toEntity());
 		return "redirect:/tasks";  // 二重サブミット対策
 	}
--- src/main/java/com/example/todo/controller/task/TaskFrom.java
+++ src/main/java/com/example/todo/controller/task/TaskFrom.java
@@ -3,9 +3,14 @@ package com.example.todo.controller.task;
 import com.example.todo.service.task.TaskEntity;
 import com.example.todo.service.task.TaskStatus;
 
+import jakarta.validation.constraints.NotBlank;
+
+
 public record TaskFrom(
+		@NotBlank //79: バリデーション 必須入力
 		String summary,
 		String description,
+		@NotBlank
 		String status
 		) {
 
