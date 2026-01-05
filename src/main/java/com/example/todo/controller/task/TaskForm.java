684f0aeb7637a17ec7f570a844581919dd53ead5
Resaku9 <rariariara17@gmail.com>
2026-01-05 10:35:13 +0900

    83: 概要の入力欄に最大長のバリデーションを追加

--- src/main/java/com/example/todo/controller/task/TaskForm.java
+++ src/main/java/com/example/todo/controller/task/TaskForm.java
@@ -4,10 +4,12 @@ import com.example.todo.service.task.TaskEntity;
 import com.example.todo.service.task.TaskStatus;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 
 public record TaskForm(
 		@NotBlank //79: バリデーション 必須入力
+		@Size(max = 256, message = "256文字以内で入力してください。")
 		String summary,
 		String description,
 		@NotBlank
